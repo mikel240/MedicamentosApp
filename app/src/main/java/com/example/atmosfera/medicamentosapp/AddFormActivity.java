56345f8cc8f5fdf1688c63e27963ae9b91fd5b5e
mikel240 <miguel_real_2@hotmail.com>
2016-04-23 14:36:50 +0100

    Form añadir medicamento

--- app/src/main/java/com/example/atmosfera/medicamentosapp/AddFormActivity.java
+++ app/src/main/java/com/example/atmosfera/medicamentosapp/AddFormActivity.java
@@ -1,14 +1,25 @@
 package com.example.atmosfera.medicamentosapp;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.example.atmosfera.medicamentosapp.adapters.ViewPagerAdapter;
+import com.example.atmosfera.medicamentosapp.fragments.DashboardFragment;
+import com.example.atmosfera.medicamentosapp.fragments.Image1SliderFragment;
+import com.example.atmosfera.medicamentosapp.fragments.Image2SliderFragment;
+import com.example.atmosfera.medicamentosapp.fragments.Image3SliderFragment;
+import com.example.atmosfera.medicamentosapp.fragments.RegistroFragment;
+import com.example.atmosfera.medicamentosapp.fragments.SettingsFragment;
+import com.example.atmosfera.medicamentosapp.fragments.TomarHoyFragment;
+
 public class AddFormActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,12 @@ public class AddFormActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+
     }
 
     @Override
@@ -32,4 +49,15 @@ public class AddFormActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Image1SliderFragment(),"pastillas");
+        adapter.addFragment(new Image2SliderFragment(), "inyeccion");
+        adapter.addFragment(new Image3SliderFragment(), "jarabe");
+
+        viewPager.setAdapter(adapter);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/atmosfera/medicamentosapp/fragments/Image2SliderFragment.java
@@ -0,0 +1,20 @@
+package com.example.atmosfera.medicamentosapp.fragments;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.atmosfera.medicamentosapp.R;
+
+public class Image2SliderFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.image2_slider_fragment,container,false);
+    }
+}
