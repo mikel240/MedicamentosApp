50ce6d0803ec255adef9db7fa637a82df9396d76
mikel240 <miguel_real_2@hotmail.com>
2016-04-26 12:41:26 +0100

    land layouts 1

--- app/src/main/java/com/example/atmosfera/medicamentosapp/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/example/atmosfera/medicamentosapp/adapters/ViewPagerAdapter.java
@@ -10,12 +10,14 @@ import com.example.atmosfera.medicamentosapp.fragments.SettingsFragment;
 import com.example.atmosfera.medicamentosapp.fragments.TomarHoyFragment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     private final List<Fragment> mFragmentList = new ArrayList<>();
     private final List<String> mFragmentTitleList = new ArrayList<>();
+    private final HashMap<Fragment,String> FragmentTitleHash = new HashMap<Fragment,String>();
 
     public ViewPagerAdapter(FragmentManager fm) {
         super(fm);
@@ -34,10 +36,15 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     public void addFragment(Fragment fragment, String title) {
         mFragmentList.add(fragment);
         mFragmentTitleList.add(title);
+        FragmentTitleHash.put(fragment,title);
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
         return mFragmentTitleList.get(position);
+    }
+
+    public String getTitleFromFragment(Fragment fragment){
+        return FragmentTitleHash.get(fragment);
     }
 }
